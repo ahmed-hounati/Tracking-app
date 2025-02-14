383d88c3dbafc24d5932be077e2e23efa7132e66
hounati-ahmed <hounatiahmed@gmail.com>
2025-02-14 09:11:19 +0100

    fc

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost:27017/tracking'),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
